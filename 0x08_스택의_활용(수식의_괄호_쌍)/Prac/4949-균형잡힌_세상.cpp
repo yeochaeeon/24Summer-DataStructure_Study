f88683e69a80cd72309e33d05553c7e736acb555
yeochaeeon <codjs2659@pusan.ac.kr>
2024-07-27 05:26:52 +0900

    Update - BOJ

--- 0x08_스택의_활용(수식의_괄호_쌍)/Prac/4949-균형잡힌_세상.cpp
+++ 0x08_스택의_활용(수식의_괄호_쌍)/Prac/4949-균형잡힌_세상.cpp
@@ -3,21 +3,47 @@
 #include <stack>
 
 int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr); 
+    
     std::stack<char> s;
     std::string str,ans;
-    while ( std::getline(std::cin,str)) {
+    while ( std::getline(std::cin,str) ) {
         if ( str == ".") break; 
         for(char c:str){
-            if ( c == '[' || c == '{' ) {
+            if ( c == '[' || c == '{' || c == '(') {
                 s.push(c);
             }
-            else if (c == ']' )  {
-                if ( s.top() == '[') s.pop();
-                else {ans = "no";}
+            else if ( c == ']' )  {
+                if ( s.size() != 0 && s.top() == '[') s.pop();
+                else {
+                    ans = "no\n";
+                    break;
+                }
             }
             else if ( c == '}') {
-                if ( s.top() == '{') s.pop();
+                if ( s.size() != 0 && s.top() == '{') {
+                    s.pop();
+                }
+                else {
+                    ans = "no\n";
+                    break;
+                }
             }
+            else if ( c == ')') {
+                if (  s.size() != 0 && s.top() == '(') {
+                    s.pop();
+                }
+                else {
+                    ans = "no\n";
+                    break;
+                }
+            }
+            if (s.size() == 0) ans = "yes\n";
+            else ans = "no\n";
         }
-    }
+        std::cout << ans ;
+        while(!s.empty()) s.pop();
+    } 
+    return 0;
 }
